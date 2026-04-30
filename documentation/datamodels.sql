1ed493f9785f62137574c2120c791d818514fe47
Funato-san <dreuzzftw@gmail.com>
2026-04-30 12:48:49 +0200

    Added datamodels

---
+++ documentation/datamodels.sql
@@ -0,0 +1,21 @@
+-- ALBUMS --
+
+
+-- ARTISTS -- 
+
+-- GENRES (er den nødvendig?) -- 
+
+-- MOODS -- 
+
+-- TRACKS & MOODS -- 
+
+-- TRACKS -- 
+
+-- VOTES -- 
+
+-- USERS -- 
+
+-- SESSIONS -- 
+
+-- SESSIONS_TRACKS -- 
+
