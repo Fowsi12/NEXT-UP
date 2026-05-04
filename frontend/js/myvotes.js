7f5e7ebbf5bf2806da0b9d1f64f2ee67ed00a733
Funato-san <dreuzzftw@gmail.com>
2026-05-04 14:41:53 +0200

    Menu drop down, MyVotes side.
    Co-authored-by: Faysal Faraah <fayzf0028@gmail.com>
    Co-authored-by: Fawsi Ahmed Mohamed Abdi <Fowsi337@hotmail.com>

---
+++ frontend/js/myvotes.js
@@ -0,0 +1,30 @@
+/* TO DO: RYD OP PÅ DENNE SIDE FAY!!!!!!! */
+
+const myVotesList = document.getElementById("myVotes");
+
+        function vote (song){
+            const li = document.createElement("li");
+            li.textContent = song;
+            myVotesList.appendChild(li); /*appendChild betyder(tilføj noget inde i noget andet til sidst*/
+        }
+
+        function removeSong(button){
+            const li = button.parentElement;
+            li.remove();
+        }
+    /*slet sang fra listen*/
+    function removeSong(button){
+        const li = button.parentElement.parentElement;
+        li.remove(); 
+    }
+    //fortryd sidste vote
+    function undoLastVote(){
+        const last = myVotesList.lastElementChild;
+        if(last){
+            last.remove();
+        }
+    }
+    // bekræft
+    function confirmVotes(){
+        
+    }
--- frontend/js/ui.js
+++ frontend/js/ui.js
@@ -1,4 +1,6 @@
 /* OPEN MOOD BOXES */
+let isMoodBoxOpen = null // variabel til at se, om MoodBox er åben eller lukket
+console.log("isMoodBoxOpen: " + isMoodBoxOpen)
 const moodBoxes = document.querySelectorAll(".moodBox");
 const openMoodBoxButtons = document.querySelectorAll(".openMoodBox");
 /* .querySelectorAll:
@@ -8,35 +10,28 @@ openMoodBoxButtons.forEach(function(button) {
 /* forEach: 
 følgende gøres for hvert element i NodeList'en*/
     button.addEventListener("click", function() {
-        
-        moodBoxes.forEach(function(box) {
-            box.style.display = "none";
-            box.style.opacity = "0";
-            box.style.pointerEvents = "none";
-        });
 /*moodBoxes.forEach:
 Lukker evt. åbne Moodboxes, hvis man klipper på en anden*/
-
         const targetBox = button.dataset.target;
 /* button.dataset.target:
 const targetBox oprettes fra attributten "data-target" fra html 
 I html har hver knap sit eget data-target, så de kan pege på forskellige classes*/
         const box = document.getElementById(targetBox);
-/* const box: 
-Det er den box, som vi vil ændre på (åbne).
-Den rammer vi vha. getElementById(targetBox). 
-I stedet for (targetBox), kunne vi have skrevet en class på en box, 
-men vi har 6 boxe, som skal åbnes med hver deres knap... 
-På denne måde kan vi ramme alle 6 med én funktion*/
         box.style.display = "block";
-
+        isMoodBoxOpen = true
+        console.log("isMoodBoxOpen: " + isMoodBoxOpen)
         setTimeout(function () {
             box.style.opacity = "1";
             box.style.pointerEvents = "auto";
         }, 200);
     });
 });
-
+/* const box: 
+Det er den box, som vi vil ændre på (åbne).
+Den rammer vi vha. getElementById(targetBox). 
+I stedet for (targetBox), kunne vi have skrevet en class på en box, 
+men vi har 6 boxe, som skal åbnes med hver deres knap... 
+På denne måde kan vi ramme alle 6 med én funktion*/
 
 /* CLOSE MOOD BOXES */
 const closeMoodBoxButtons = document.querySelectorAll(".closeMoodBox"); 
@@ -60,7 +55,7 @@ Først tjekkes elementet selv, så parent og så parent's parent osv.
 Derfor finder den den moodBox, som knappen er i og udfører nedenstående*/
     setTimeout(function() {
         moodBox.style.opacity = "0";
-        box.style.pointerEvents = "none"
+        moodBox.style.pointerEvents = "none"
     }, 200);
 /* setTimeout: 
 venter x milliseconds, 
@@ -69,6 +64,8 @@ så man kan se knappen reagere*/
     setTimeout(function() {
         moodBox.style.display = "none";
     }, 500);
+    isMoodBoxOpen = false
+    console.log("isMoodBoxOpen: " + isMoodBoxOpen)
 /* setTimeout: 
 venter 500 ms inden boxen bliver helt skjult igen, 
 fordi den har style="transition: opacity 0.5s"*/
